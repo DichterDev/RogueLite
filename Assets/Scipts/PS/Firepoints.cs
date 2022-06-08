8cf47abcc250edfe047bfa723d4a333ba2708985
Dichter <david.dr.ruescher@outlook.com>
2022-06-08 20:49:21 +0200

    finished ps

--- Assets/Scipts/PS/Firepoints.cs
+++ Assets/Scipts/PS/Firepoints.cs
@@ -7,20 +7,22 @@ public class Firepoints : MonoBehaviour
     public ProjectileSpammerManager manager;
 
     float rotation = 360;
-    Vector3 tempRot = new Vector3();
+    Vector3 pos = new Vector3();
 
-    private void Awake()
+    private void Start()
     {
         rotation = rotation / manager.Firepoints;
+        pos = transform.TransformPoint(new Vector3(0, 0.5f));
 
         for (int i = 0; i < manager.Firepoints; i++)
         {
-            GameObject go = Instantiate(new GameObject());
+            GameObject go = new GameObject();
+            go.transform.position = pos;
+            go.transform.rotation = Quaternion.Euler(0, 0, 0);
             go.tag = "FirePoint";
             go.transform.parent = transform;
-            go.transform.position = new Vector3(0, 0.5f);
 
-            gameObject.transform.rotation = Quaternion.Euler(tempRot);
-        }    
+            gameObject.transform.Rotate(0, 0, rotation);
+        }
     }
 }
--- Assets/Scipts/PS/ProjectileSpammerManager.cs
+++ Assets/Scipts/PS/ProjectileSpammerManager.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Spammer", fileName = "Manager", order = 1)]
 public class ProjectileSpammerManager : ScriptableObject
 {
-    public GameObject Projectile;
+    public GameObject Projectile; // Projectile Prefab
 
-    public int Firepoints;
-    public int ProjDamage;
-    public float ProjSpeed;
-    public float ProjForce;
-    public float ProjTime;
-    public float FireRate;
+    public int Firepoints; // Number of Firepoints
+    public int ProjDamage; // Damage of Projectile
+    public float ProjSpeed; // Travel speed of Projectile
+    public float ProjForce; // Initital Force of Projectile
+    public float ProjTime; // absolute time until destruction of Projectile
+    public float FireRate; // Projectiles per Second
+    public int SpinSpeed; // Degrees per Second
 }
