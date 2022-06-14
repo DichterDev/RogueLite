e0ecc46c86ec2eb405ec842c2caae2672f7ef023
Wario19 <konstantin.zerlauth@gmx.at>
2022-06-14 11:10:29 +0200

    bullets

---
+++ Assets/Scipts/GameSwitchRoom.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSwitchRoom : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public string SwitchToRoom;
+    public float delay;
+
+    void Start()
+    {
+
+       
+        
+    }
+
+   
+
+   
+}
